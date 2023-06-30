f3c07d28c33554f8a6457ae02ffe09a813c5f9a6
kdj <es-kbsys24@hybecorp.com>
2023-07-01 00:31:21 +0900

    Narrowing, Assertion 학습 문제 풀이 예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { type } from 'os';
 
 function App() {
   let test:string = "test입니다"
@@ -195,7 +196,58 @@ function 숙제3(a:number,b:boolean,c:string){
 }
 
 숙제3(700, false, '상');
+
+
+/* Narrowing & Assertion */
+/* 유니언 타입 같이 타입이 아직 확정되지 않았을 경우 Type Narrowing을 써야합니다. */
   
+function 내함수(x:string|number) { 
+  if(typeof x === "string"){
+    return x + "1";
+  }else{
+    return x + 1;
+  }
+  //어떤 변수 타입이 아직 불확실 하다면 if문으로 Narrowing을 해줘야 조작 가능
+  //Narrowing을 할 때 주의점 if문을 끝까지 작성해야함
+  
+  //Narrowing으로 판정을 해주는 문법들
+  //typeof 변수, 속성명 in 오브젝트 자료, 인스턴스 instanceof 부모
+
+  //Narrowing을 쓰기 싫으면 Assertion 어썰션(타입을 한번 덮어쓰기)
+
+}   
+function 내함수2(x:string|number) { 
+let arr:number[]=[];
+arr[0] = x as number;
+//as number는 as뒤 타임을 x에 덮어쓴다.
+//Assertion 문법의 용도
+//1. Narrowing을 하려고 쓴다.
+//2. 타입을 변경시키는 것이 아닌 여러개의 유니언 타입을 하나로 통일하는 것이다.
+//3. 어떤 타입이 확실하게 들어올지 100% 알 때 쓰셈   as 문법 
+//4. 남의 코드 고칠때
+}
+내함수(2);
+내함수2(2)
+
+
+//(숙제1) 숫자여러개를 array 자료에 저장해놨는데
+//가끔 '4', '5' 이런 식의 문자타입의 숫자가 발견되고 있습니다.
+//이걸 클리닝해주는 함수가 필요합니다. 
+//클리닝함수( ['1', 2, '3'] ) 이렇게 숫자와 문자가 섞인 array를 입력하면
+//[1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
+//모르는 부분은 구글검색해도 봐드림 
+
+let 철수쌤 = { subject : 'math' }
+let 영희쌤 = { subject : ['science', 'english'] }
+let 민수쌤 = { subject : ['science', 'art', 'korean'] }
+
+//지금 여러 변수에 선생님이 가르치고 있는 과목이 저장이 되어있습니다. 
+//과목 1개만 가르치는 쌤들은 문자 하나로 과목이 저장이 되어있고
+//과목 2개 이상 가르치는 쌤들은 array 자료로 과목들이 저장되어있습니다. 
+//철수쌤같은 선생님 object 자료를 집어넣으면 
+//그 선생님이 가르치고 있는 과목중 맨 뒤의 1개를 return 해주는 함수를 만들어봅시다.
+//그리고 타입지정도 엄격하게 해보도록 합시다
+
 return (
     <div className="App">
       타입 스크립트
